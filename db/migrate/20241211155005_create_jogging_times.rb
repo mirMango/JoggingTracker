05603097eda60d46da1ac6cf2d2ea28959c461d1
David <davidionut.miron@gmail.com>
2024-12-11 17:52:00 +0200

    jogging time scaffold

---
+++ db/migrate/20241211155005_create_jogging_times.rb
@@ -0,0 +1,12 @@
+class CreateJoggingTimes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :jogging_times do |t|
+      t.date :date
+      t.float :distance
+      t.time :time
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,25 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_12_11_155005) do
+  create_table "jogging_times", force: :cascade do |t|
+    t.date "date"
+    t.float "distance"
+    t.time "time"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_jogging_times_on_user_id"
+  end
+
+  add_foreign_key "jogging_times", "users"
+end
